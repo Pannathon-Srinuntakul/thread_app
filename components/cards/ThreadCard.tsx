313ce6fc8ca0a0be7993068d61bad00c5ce7f081
Beam <pabeam27@gmail.com>
2024-05-15 16:47:50 +0700

    updated

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,4 @@
+import { formatDateString } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -34,10 +35,16 @@ const ThreadCard = ({
   community,
   createdAt,
   comments,
-  isComment
+  isComment,
 }: Props) => {
   return (
-    <article className={` ${isComment ? 'px-0 xs:px-7' : 'flex w-full flex-col rounded-xl bg-dark-2 p-7'}`}>
+    <article
+      className={` ${
+        isComment
+          ? "px-0 xs:px-7"
+          : "flex w-full flex-col rounded-xl bg-dark-2 p-7"
+      }`}
+    >
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
           <div className="flex flex-col items-center">
@@ -61,7 +68,7 @@ const ThreadCard = ({
             </Link>
             <p className="mt-2 text-small-regular text-light-2">{content}</p>
 
-            <div className={`${isComment && 'mb-10'} mt-5 flex flex-col gap-3`}>
+            <div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
               <div className="flex gap-3.5">
                 <Image
                   src="/assets/heart-gray.svg"
@@ -98,13 +105,34 @@ const ThreadCard = ({
 
               {isComment && comments.length > 0 && (
                 <Link href={`/thread/${id}`}>
-                  <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} replies
+                  </p>
                 </Link>
               )}
             </div>
           </div>
         </div>
+        {/* TODO Delete thread */}
+        {/* TODO Show Comment logos */}
+
       </div>
+        {!isComment && community && (
+          <Link href={`/communities/${community.id}`} className="mt-5 flex items-center"> 
+          <p className="text-subtle-medium text-gray-1">
+            {formatDateString(createdAt)}
+            {' '} - {community.name} Community
+          </p>
+
+          <Image 
+          src={community.image}
+          alt={community.name}
+          width={14}
+          height={14}
+          className="ml-1 rounded-full object-cover"
+          />
+          </Link>
+        )}
     </article>
   );
 };
